460b54db383a545278859e7d2c1d1aff4603de66
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-16 16:57:35 +0430

    Adds 2-professional-chess.py

---
+++ Quera/fundamentals-of-programming-and-algorithmic-thinking/2-professional-chess.py
@@ -0,0 +1,8 @@
+# Problem: https://quera.org/problemset/2636
+
+input_pieces = list(map(int, input().split()))
+
+valid_pieces = [1, 1, 2, 2, 2, 8]
+
+for i in range(len(valid_pieces)):
+    print(valid_pieces[i] - input_pieces[i], end=" ")
