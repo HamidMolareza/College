2ccc7dd744b7484f77b27f0246eb842b473057a5
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-28 14:46:17 +0330

    Adds 11-list-in-python.py

---
+++ Quera/fundamentals-of-programming-and-algorithmic-thinking/11-list-in-python.py
@@ -0,0 +1,6 @@
+user_list = [int(x) for x in input("Enter multiple numbers with a space: ").split()]
+
+print("List:", user_list)
+print("Index 1:", user_list[1])
+print("Length:", len(user_list))
+print("Repeat list 2 times:", user_list * 2)
