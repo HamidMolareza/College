aca6ac03d2aae67afa019296fb3d869e88c07601
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-06 19:47:49 +0330

    overrides SaveChanges

--- Mizfa/asp-core/1-CRUD-Persons-Admin/WebApi/WebApi/Data/PersonContext.cs
+++ Mizfa/asp-core/1-CRUD-Persons-Admin/WebApi/WebApi/Data/PersonContext.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using WebApi.Models.Db;
 
 namespace WebApi.Data;
@@ -10,4 +12,46 @@ public class PersonContext : DbContext {
     public DbSet<EmailDb> Emails { get; set; }
     public DbSet<PhoneDb> Phones { get; set; }
     public DbSet<ProfileImageDb> ProfileImages { get; set; }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new()) {
+        UpdateEntries(ChangeTracker);
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public override int SaveChanges() {
+        UpdateEntries(ChangeTracker);
+        return base.SaveChanges();
+    }
+
+    //TODO: Test
+    private static void UpdateEntries(ChangeTracker changeTracker) {
+        var now = DateTime.UtcNow;
+
+        foreach (var changedEntity in changeTracker.Entries()) {
+            if (changedEntity.Entity is BaseDb baseObj) {
+                switch (changedEntity.State) {
+                    case EntityState.Added:
+                        baseObj.Created = now;
+                        baseObj.Updated = now;
+                        break;
+                    case EntityState.Modified:
+                        baseObj.Updated = now;
+                        break;
+                }
+            }
+            
+            foreach (var propertyInfo in changedEntity.Entity.GetType().GetProperties()) {
+                if (propertyInfo.PropertyType == typeof(DateTime)) {
+                    ConvertToUtc(changedEntity.Entity, propertyInfo);
+                }else if (propertyInfo.PropertyType == typeof(DateTime?) &&
+                          ((DateTime?) propertyInfo.GetValue(changedEntity.Entity)).HasValue) {
+                    ConvertToUtc(changedEntity.Entity, propertyInfo);
+                }
+            }
+        }
+    }
+
+    private static void ConvertToUtc(object entity, PropertyInfo propertyInfo) {
+        propertyInfo.SetValue(entity, DateTime.SpecifyKind((DateTime) propertyInfo.GetValue(entity)!, DateTimeKind.Utc));
+    }
 }
