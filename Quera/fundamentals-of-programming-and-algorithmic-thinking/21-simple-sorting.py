9a75f1d33f7fc606d89a6f34fb23c5ab81f9d93b
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-16 02:33:11 +0330

    Adds insertion_sort & fixes bugs

--- Quera/fundamentals-of-programming-and-algorithmic-thinking/21-simple-sorting.py
+++ Quera/fundamentals-of-programming-and-algorithmic-thinking/21-simple-sorting.py
@@ -5,31 +5,51 @@ def selection_sort(numbers: List[int], reverse=False) -> List[int]:
     if len(numbers) < 1:
         return numbers
 
-    for i in range(len(numbers) - 1):
-        target_number = numbers[i]
+    values = numbers.copy()
+
+    for i in range(len(values) - 1):
+        target_number = values[i]
         target_index = i
-        for j in range(i + 1, len(numbers)):
-            if (not reverse and numbers[j] < target_number) or (reverse and numbers[j] > target_number):
-                target_number = numbers[j]
+        for j in range(i + 1, len(values)):
+            if (not reverse and values[j] < target_number) or (reverse and values[j] > target_number):
+                target_number = values[j]
                 target_index = j
         if target_index != i:
-            numbers[i], numbers[target_index] = numbers[target_index], numbers[i]
-    return numbers
+            values[i], values[target_index] = values[target_index], values[i]
+    return values
 
 
 def bubble_sort(numbers: List[int], reverse=False) -> List[int]:
-    for i in range(len(numbers) - 1):
-        for j in range(i + 1, len(numbers)):
-            if (not reverse and numbers[i] > numbers[j]) or (reverse and numbers[i] < numbers[j]):
-                numbers[i], numbers[j] = numbers[j], numbers[i]
-    return numbers
+    values = numbers.copy()
+    for i in range(len(values) - 1):
+        for j in range(i + 1, len(values)):
+            if (not reverse and values[i] > values[j]) or (reverse and values[i] < values[j]):
+                values[i], values[j] = values[j], values[i]
+    return values
+
+
+def insertion_sort(numbers: List[int], reverse=False) -> List[int]:
+    values = numbers.copy()
+    for i in range(len(values) - 1):
+        for j in range(i + 1, 0, -1):
+            if (not reverse and values[j] > values[j - 1]) or (reverse and values[j] < values[j - 1]):
+                break
+            values[j], values[j - 1] = values[j - 1], values[j]
+    return values
+
+
+def print_list(numbers: List[int]):
+    for number in numbers:
+        print(number, end=" ")
+    print()
 
 
 def main():
     input()  # num of numbers (skip)
     numbers = list(map(int, input().split()))
-    print(selection_sort(numbers, False))
-    print(bubble_sort(numbers, False))
+    print_list(selection_sort(numbers, True))
+    print_list(bubble_sort(numbers, True))
+    print_list(insertion_sort(numbers, True))
 
 
 main()
