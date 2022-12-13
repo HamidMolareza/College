dd5d2daaa9943218597af3453450698dfb64c6bd
Hamid Molareza <kitm.hamid@gmail.com>
2022-12-13 22:49:37 +0330

    Adds Appbar to App.tsx in DesignCourse/2

--- DesignCourse/2-cards-buttons/src/App.tsx
+++ DesignCourse/2-cards-buttons/src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import HideAppBar from "./components/Appbar";
-import {Box, Container} from '@mui/material';
+import {Box, Container, Typography} from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 export default function App() {
     return (
-        <div>
-            <HideAppBar/>
+        <>
+            <Box sx={{
+                display: ["block", "block", "none"]
+            }}>
+                <HideAppBar>
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        margin: "auto",
+                        gap: "10px"
+                    }}>
+                        <ArrowRightAltIcon sx={{
+                            transform: "rotate(180deg)",
+                            width: "35px",
+                            height: "35px"
+                        }}/>
+                        <Typography variant="h6">
+                            1 of 5
+                        </Typography>
+                        <ArrowRightAltIcon sx={{
+                            width: "35px",
+                            height: "35px"
+                        }}/>
+                    </Box>
+                </HideAppBar>
+            </Box>
             <Container>
                 <Box sx={{my: 2}}>
                     {[...new Array(30)]
@@ -18,6 +44,6 @@ Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
                         .join('\n')}
                 </Box>
             </Container>
-        </div>
+        </>
     );
 }
