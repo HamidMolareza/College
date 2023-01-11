119d213eb6708d03240f3c40f7cb5834931d6249
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-11 11:39:04 +0330

    Init Navbar in DesignCourse

---
+++ DesignCourse/3-navbar/src/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Box, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+
+declare module '@mui/material/styles' {
+    interface BreakpointOverrides {
+        xs: false; // removes the `xs` breakpoint
+        sm: false;
+        md: false;
+        lg: false;
+        xl: false;
+        mobile: true; // adds the `mobile` breakpoint
+        tablet: true;
+        laptop: true;
+        desktop: true;
+        fourK: true;
+    }
+}
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            mobile: 0,
+            tablet: 768,
+            laptop: 1024,
+            desktop: 1536,
+            fourK: 2560,
+        },
+    },
+});
+
+
+root.render(
+    <React.StrictMode>
+        <CssBaseline/>
+        <ThemeProvider theme={theme}>
+            <Box sx={{backgroundColor: "#EEEEEE"}}>
+                <App/>
+            </Box>
+        </ThemeProvider>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
